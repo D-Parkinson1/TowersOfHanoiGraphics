77011f1bedd5352fffc121b33a8f00bc3491edbe
D-Parkinson1 <24710205+D-Parkinson1@users.noreply.github.com>
2021-06-07 11:51:04 +1000

    Store map type in texture

--- Texture.py
+++ Texture.py
@@ -4,15 +4,20 @@ from PIL import Image
 
 class Texture:
 
-    def __init__(self, filePath, textureType=GL_TEXTURE_2D):
+    def __init__(self, filePath, textureGLType=GL_TEXTURE_2D, mapType=None):
         with Image.open(filePath) as image:
             self.mode = "RGBX" if image.mode == 'RGB' else "RGBA"
             data = image.tobytes("raw", self.mode, 0, -1)
             self.width = image.size[0]
             self.height = image.size[1]
 
+        if mapType in ("diffuse", "specular", "emmision"):
+            self.mapType = mapType
+        else:
+            self.mapType = None
+
         self.texture = glGenTextures(1)
-        glBindTexture(textureType, self.texture)
+        glBindTexture(textureGLType, self.texture)
 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
@@ -20,14 +25,14 @@ class Texture:
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
 
         if (data):
-            glTexImage2D(textureType, 0, GL_RGBA, self.width, self.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data)
-            glGenerateMipmap(textureType)
+            glTexImage2D(textureGLType, 0, GL_RGBA, self.width, self.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data)
+            glGenerateMipmap(textureGLType)
         else:
             print("WARNING: FAILED to load texture '%s'" % filePath)
             raise RuntimeWarning
 
         glActiveTexture(GL_TEXTURE0)
-        glBindTexture(textureType, 0)
+        glBindTexture(textureGLType, 0)
         data = None
 
     def delete(self):
